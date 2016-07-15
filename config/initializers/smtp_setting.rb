6b4f483a8434dc19fc1a94095d14502b0d481ea7
Mbhati-Zensar <mukesh.bhati@zensar.in>
2016-07-15 20:44:45 +0530

    gmail changes

--- config/initializers/smtp_setting.rb
+++ config/initializers/smtp_setting.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "gmail.com",
-  :user_name            => "spa911forcar@gmail.com",
-  :password             => "spa911forcar@123",
-  :authentication       => "login",
+	:address => "smtp.gmail.com",
+  :port => 587,
+  :domain => 'http://spa911.herokuapp.com/',
+  :user_name => "spa911forcar@gmail.com",
+  :password => "spa911forcar123",
+  :authentication => :plain,
   :enable_starttls_auto => true
 }
 
