725150b3a48b374de64500019a85124b96229097
Mbhati-Zensar <mukesh.bhati@zensar.in>
2017-08-20 01:09:53 +0530

    SEO Basic Changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server 'ec2-35-167-248-199.us-west-2.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
+server 'ec2-35-163-5-192.us-west-2.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
 
 
 # role-based syntax
